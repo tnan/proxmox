827d52d2d371640b2175331e166cd820505562b1
tnan <mr.tnan@live.com>
2023-06-15 01:10:43 -0700

    Update enable_root_ssh.sh

--- enable_root_ssh.sh
+++ enable_root_ssh.sh
@@ -4,4 +4,7 @@ sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 sed -i "s/.*PubkeyAuthentication.*/PubkeyAuthentication no/g" /etc/ssh/sshd_config
 sed -i "s/.*PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 
+rm /root/rclocal.sh
+sed -i "s/.*sh /root/rclocal.sh.*//g" /etc/ssh/sshd_config
+
 poweroff
