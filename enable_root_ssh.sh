a060480b8d63725a6e358867911ec2300fa32719
tnan <mr.tnan@live.com>
2023-06-15 00:30:03 -0700

    Update enable_root_ssh.sh

--- enable_root_ssh.sh
+++ enable_root_ssh.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-sleep 10
 
 sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/g" /etc/ssh/sshd_config
 sed -i "s/.*PubkeyAuthentication.*/PubkeyAuthentication no/g" /etc/ssh/sshd_config
 sed -i "s/.*PasswordAuthentication.*/PasswordAuthentication yes/g" /etc/ssh/sshd_config
-sed -i "s/.*ssh_pwauth.*/ssh_pwauth: true/" /etc/cloud/cloud.cfg
-sed -i "s/.*disable_root.*/disable_root: false/g" /etc/cloud/cloud.cfg
 
 poweroff
