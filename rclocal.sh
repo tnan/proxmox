e85d6e6cbddeae5e864ab058e2a5f0ef02fa6557
tnan <mr.tnan@live.com>
2023-06-15 23:49:41 -0700

    Update rclocal.sh

--- rclocal.sh
+++ rclocal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "bash <(curl -s https://raw.githubusercontent.com/tnan/proxmox/main/enable_root_ssh.sh)" >> /etc/rc.local
+echo "curl -s https://raw.githubusercontent.com/tnan/proxmox/main/enable_root_ssh.sh | sh)" >> /etc/rc.local
 chmod +x /etc/rc.local
 systemctl enable rc-local
 reboot
