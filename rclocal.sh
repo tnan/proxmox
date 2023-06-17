54552acbd14bca6c17071385613ad060ed36b08b
tnan <mr.tnan@live.com>
2023-06-16 22:58:15 -0700

    Update rclocal.sh

--- rclocal.sh
+++ rclocal.sh
@@ -7,7 +7,7 @@ sed -i 's/PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PubkeyAuthentication.*/PubkeyAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
-sed -i 's/sh \/root\/rclocal.sh//g' /etc/rc.local
+echo "" > /etc/rc.local
 rm /root/rclocal.sh
 
 poweroff
