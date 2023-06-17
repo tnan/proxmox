b7116e23baba3d830c74c3243332306f5c4a0391
tnan <mr.tnan@live.com>
2023-06-16 23:13:51 -0700

    Update rclocal.sh

--- rclocal.sh
+++ rclocal.sh
@@ -3,9 +3,9 @@
 sed -i 's/.*disable_root.*/disable_root: false/g' /etc/cloud/cloud.cfg
 sed -i 's/.*ssh_pwauth.*/ssh_pwauth: true/g' /etc/cloud/cloud.cfg
 
-sed -i 's/PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/PubkeyAuthentication.*/PubkeyAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/.*PubkeyAuthentication.*/PubkeyAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 echo "" > /etc/rc.local
 rm /root/rclocal.sh
