2f8d1274e312c3d14c39623bbd3f6ee0d192354a
tnan <mr.tnan@live.com>
2023-06-16 23:37:22 -0700

    Update rclocal.sh

--- rclocal.sh
+++ rclocal.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-sed -i 's/.*disable_root.*/disable_root: false/g' /etc/cloud/cloud.cfg
-sed -i 's/.*ssh_pwauth.*/ssh_pwauth: true/g' /etc/cloud/cloud.cfg
+sed -i 's/disable_root.*/disable_root: false/g' /etc/cloud/cloud.cfg
+sed -i 's/ssh_pwauth.*/ssh_pwauth: true/g' /etc/cloud/cloud.cfg
 
-sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sed -i 's/.*PubkeyAuthentication.*/PubkeyAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/PubkeyAuthentication.*/PubkeyAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/KbdInteractiveAuthentication.*/KbdInteractiveAuthentication yes/g' /etc/ssh/sshd_config
+sed -i 's/ChallengeResponseAuthentication .*/ChallengeResponseAuthentication yes/g' /etc/ssh/sshd_config
 
 echo "" > /etc/rc.local
 rm /root/rclocal.sh
